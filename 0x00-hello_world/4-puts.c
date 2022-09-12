e10b5992bf0d9f1ebf7e55534f1816f4b6337ab1
lafe-poem <makanjuomojesu@gmail.com>
2022-09-12 09:37:50 -0700

    my new c programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - prints the string in the put function 
- *
- * Description: using the main function
- * this program prints "programming is like building a multilingual puzzle
- * return: 0
+ * main - prints the phrase "programming is like building a multilingual puzzle 
+ * return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {  
